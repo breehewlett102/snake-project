680eaaa97f04aa3d9cff30d9da48b373fa5a25ac
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-08 19:36:38 -0700

    Initial commit

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const snakeNames = require('snake-names')
 let namePicked = snakeNames.random()
 
 console.log('You should name this snake:', namePicked)
+
+let femaleSnakeNames = snakeNames.female
+
+let randomIndex = Math.floor(Math.random() * femaleSnakeNames.length)
+
+console.log("A great female snake name is:", femaleSnakeNames[randomIndex])
